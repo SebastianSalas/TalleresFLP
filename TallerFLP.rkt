1f2546bd6fc42993298524db12dd14b5ad9c3f81
Sebastian Salas <janiersalas09@gmail.com>
2023-03-27 00:24:16 -0500

    Adds first point

--- TallerFLP.rkt
+++ TallerFLP.rkt
@@ -1,5 +1,23 @@
 #lang eopl
 
+
+(define invert 
+  (lambda(l)
+    (if(null? l)
+      '()
+      (append invert-aux(car lista) invert(l))
+    )
+  )
+)
+
+(define invert-aux
+  (lambda(l)
+    (cond[(null? l) '()]
+    [else cons(cons((cdr (car l)) (car l)) (invert-aux (cdr l)))]
+    ))
+)
+
+
 (define suma (lambda (x y) (+ x y)))
 
 (define invert
