1e97d87e392555d204fb6e1ae8f167e1820b14ab
AlexanderValencia21 <alexandervalencia0121@gmail.com>
2023-05-11 17:44:52 -0500

    Adding the lexical specification

--- solucionTaller3.rkt
+++ solucionTaller3.rkt
@@ -1,11 +1,22 @@
 #lang eopl
 
 ;; Solución taller #3 - Fundamentos de lenguajes de programación
-;; Domingo, 11 de mayo de 2023
+;; Jueves, 11 de mayo de 2023
 ;; Hecho por:
 ;; Janiert Sebastián Salas - 1941265
 ;; Jhon Alexander Valencia - 2042426
 ;; Diego Fernando Victoria - 2125877
+
+;;Performs the lexical specification, which refers the way in wich the program is divided into lexical units
+(define lexic-specification
+  '((espacio-blanco (whitespace) skip)
+    (comentario ("->" (arbno (or digit letter #\newline whitespace))) skip)  ;The comments starts with ->
+    (identificador ("@" letter (arbno (or letter digit))) symbol)
+    (numero (digit (arbno digit)) number)
+    (numero ("-" digit (arbno digit)) number)
+    (numero (digit (arbno digit) "." digit (arbno digit)) number)
+    (numero ("-" digit (arbno digit) "." digit (arbno digit)) number)
+    (texto (letter (arbno (or letter ":" "?"))) string)))
 
 ;; Grammar of the language, which describes the rules of the language, used to define the terminal symbols, non-terminal symbols, and production rules.
 (define gramatica
