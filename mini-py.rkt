0681f5be77519ac3a42855ab735c1159fec3842f
DiegoFernando01 <diegofvictoria2013@gmail.com>
2023-05-21 11:51:48 -0500

    Adds file mini-py

---
+++ mini-py.rkt
@@ -0,0 +1,7 @@
+;; Solución proyecto final - Fundamentos de lenguajes de programación
+;; Viernes, 9 de junio de 2023
+;; Hecho por:
+;; Janiert Sebastián Salas - 1941265
+;; Jhon Alexander Valencia - 2042426
+;; Diego Fernando Victoria - 2125877
+
